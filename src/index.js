a02dc1a5abb2c1cd95850428118a2da235032f2a
shannone <shannone426@gmail.com>
2022-08-19 12:43:49 -0400

    winning"

--- src/index.js
+++ src/index.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+// tried to ask a TC for help but I dont think I even formulated my question properly and got offf the call and just cried!
+fetch('http://localhost:3000/toys')
+.then(response => response.json())
+.then(toys => {
+  toys.forEach(toy => {
+
+  })
+})
+// i need to add a div elememt and in that div create h2 img p and Btn
+function renderToyCard(toy) {
+  
+};
+// 1. (fetch Andys toys)i think this is asking me to write a function to make a card for each toy and run it right away in my forEach
+// "When the page loads, make a 'GET' request to fetch all the toy objects. With the response data, make a <div class="card"> for each toy and add it to the toy-collection div."
+
+// 2. (Add Toy Info to the Card) I am so lost!
+// document.createElement('h2')
+// document.createElement('img')
+// document.createElement('p')
+// document.createElement('button')
